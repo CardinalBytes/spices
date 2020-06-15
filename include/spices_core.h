0c33d1b9028798e84095561b7256b8263f44fbbc
Matheus Xavier <mxavier@mail.cmdforge.com>
2020-06-15 08:14:37 -0300

    add proper comments to some functions

--- include/spices_core.h
+++ include/spices_core.h
@@ -26,34 +26,51 @@ typedef enum spice_vec_tags spc_vtags_t;
 spc_vec_t *spc_veci(u64 init_size, u64 itm_size, spc_vtags_t tags);
 
 /**
- *
+ * Free the memory used by the vector
  * @param vec
  */
 void spc_vec_free(spc_vec_t *vec);
 
 /**
- *
+ * Zero fill the memory and free the vector
  * @param vec
  */
 void spc_vec_clear(spc_vec_t *vec);
 
+/**
+ * Put an item in order I.E at the end of the vector
+ * @param vec The vector to append the item to
+ * @param item_ptr Pointer to the item
+ * @return returns 0 on success
+ */
 int spc_vec_put_ord(spc_vec_t *vec, void *item_ptr);
 
+/**
+ * Get reference to item at the index
+ * @param vec Vector to get a reference to the item
+ * @param index Index to obtain the reference to
+ * @return Pointer to the item
+ */
 void *spc_vec_greft(spc_vec_t *vec, u64 index);
 
+/**
+ * Get reference to the top item of the vector
+ * @param vec Vector to operate on
+ * @return A reference to the item at the top of the vector
+ */
 void *spc_vec_popref(spc_vec_t *vec);
 
 /**
- * How many items there are in the vector
- * @param vec vector
- * @return unsigned long
+ * How many used allocations there are in the vector
+ * @param vec
+ * @return Number of used allocations in the vector
  */
 u64 spc_vec_len(spc_vec_t *vec);
 
 /**
- * How much space is allocated for the vector in bytes
+ * How much space is allocated for the vector in memory used or not
  * @param vec
- * @return
+ * @return Space that is allocated and may or may not be used
  */
 u64 spc_vec_sizeof(spc_vec_t *vec);
 
